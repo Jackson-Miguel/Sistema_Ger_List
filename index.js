df7a950840eb7034e4ca705a5cd41edc030f48fc
Jackson-Miguel <jacksonmiguelcarvalho51@gmail.com>
2025-05-24 03:01:22 +0000

    To com preguica de fazer as coisas pqp

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
-function validarForm(event){
+    const historico = [];
+    document.getElementById('Formulario').addEventListener('submit', function(event) {
     event.preventDefault();
-
-    let titulo = document.forms["Formulario"]["title"].value;
-    let datai = document.forms["Formulario"]["datei"].value;
-    let dataf = document.forms["Formulario"]["datef"].value;
-    let desc = document.forms["Formulario"]["desc"].value;
-    document.getElementById("btn_limpar").addEventListener("click", function(){
-        titulo.innerText = ""
-        datai.innerText = ""
-        dataf.innerText = ""
-        desc.innerText = ""
-    });
-    if(titulo === "" || datai === "" || dataf === "" || desc === ""){
+    const modalElement = document.getElementById('exampleModal');
+    const modal1 = bootstrap.Modal.getOrCreateInstance(modalElement);
+    const titulo = document.getElementById('title').value;
+    const datai = document.getElementById('datai').value;
+    const dataf = document.getElementById('dataf').value;
+    const desc = document.getElementById('desc').value;
+    if(titulo.trim() === "" || datai.trim() === "" || dataf.trim() === "" || desc.trim() === ""){
         alert("Preencha todos os campos.");
-        return false
     }else{
-     document.getElementById("base").innerText = "Olá, " + titulo.value + "!";
+        document.getElementById('title_res').value = titulo;
+        document.getElementById('datai_res').value = datai;
+        document.getElementById('dataf_res').value = dataf;
+        document.getElementById('desc_res').value = desc;
+        historico.push({ titulo, datai, dataf, desc });
+        modal1.hide();
     }
-}
+});
+document.getElementById("exx").addEventListener("click", function(){
+    document.getElementById("lis").remove();
+});
+function atualizarHistorico() {
+    const lista = document.getElementById('listaHistorico');
+    if (!lista) return;
+
+    lista.innerHTML = "";
+
+    historico.forEach((item, index) => {
+      const li = document.createElement("li");
+      li.className = "list-group-item";
+      li.textContent = `${index + 1}) Cão: ${item.cachorro}, Gato: ${item.gato}, Ração: ${item.racao}, Desc: ${item.desc}`;
+      lista.appendChild(li);
+    });
+  }
